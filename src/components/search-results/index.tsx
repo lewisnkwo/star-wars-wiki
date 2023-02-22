42cb31b7dfb716b8c6cf604a46c288dd9391d522
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-02-22 12:17:27 +0000

    Improve search result links

--- src/components/search-results/index.tsx
+++ src/components/search-results/index.tsx
@@ -1,3 +1,5 @@
+import { Button, ListGroup } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { Character, Planet, Starship } from "../../types";
 
 interface Props {
@@ -6,33 +8,57 @@ interface Props {
   starships: Starship[] | undefined;
 }
 
-const SearchResults = ({ characters, planets, starships }: Props) => (
-  <div>
-    {characters !== undefined && (
-      <>
-        <h3>Characters</h3>
-        {characters.length === 0 && <p>No characters found.</p>}
-        {characters !== undefined &&
-          characters.map((c, i) => <p key={i}>{c.name}</p>)}
-      </>
-    )}
-    {planets !== undefined && (
-      <>
-        <h3>Planets</h3>
-        {planets.length === 0 && <p>No planets found.</p>}
-        {planets !== undefined &&
-          planets.map((p, i) => <p key={i}>{p.name}</p>)}
-      </>
-    )}
-    {starships !== undefined && (
-      <>
-        <h3>Starships</h3>
-        {starships.length === 0 && <p>No starships found.</p>}
-        {starships !== undefined &&
-          starships.map((s, i) => <p key={i}>{s.name}</p>)}
-      </>
-    )}
-  </div>
-);
+const SearchResults = ({ characters, planets, starships }: Props) => {
+  const navigate = useNavigate();
+
+  return (
+    <ListGroup>
+      {characters !== undefined && (
+        <>
+          <h3>Characters</h3>
+          {characters.length === 0 && <p>No characters found.</p>}
+          {characters !== undefined &&
+            characters.map((c, i) => (
+              <ListGroup.Item key={i}>
+                <span className="margin-right-small">{c.name}</span>
+                <Button
+                  variant="primary"
+                  onClick={() =>
+                    navigate("/character", {
+                      state: {
+                        url: c.url,
+                      },
+                    })
+                  }
+                >
+                  View Profile
+                </Button>
+              </ListGroup.Item>
+            ))}
+        </>
+      )}
+      {planets !== undefined && (
+        <>
+          <h3>Planets</h3>
+          {planets.length === 0 && <p>No planets found.</p>}
+          {planets !== undefined &&
+            planets.map((p, i) => (
+              <ListGroup.Item key={i}>{p.name}</ListGroup.Item>
+            ))}
+        </>
+      )}
+      {starships !== undefined && (
+        <>
+          <h3>Starships</h3>
+          {starships.length === 0 && <p>No starships found.</p>}
+          {starships !== undefined &&
+            starships.map((s, i) => (
+              <ListGroup.Item key={i}>{s.name}</ListGroup.Item>
+            ))}
+        </>
+      )}
+    </ListGroup>
+  );
+};
 
 export default SearchResults;
